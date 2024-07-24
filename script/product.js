edbbaff99e6312670919da590b9653683c3b72ae
INDUWARA90 <induwaravishwakantha90@gmail.com>
2024-07-24 06:57:35 +0530

    Add List Of Product

---
+++ script/product.js
@@ -0,0 +1,549 @@
+
+// ============List of food==================
+const BurgersList = [
+    {
+        itemCode: 'B1001',
+        itemName: 'Classic Burger(Large)',
+        price: [
+            { Price:1500.00, discount:15 }
+        ]
+    },
+    {
+        itemCode: 'B1002',
+        itemName: 'Classic Burger (Regular)',
+        price: [
+            { Price:1500.00, discount:15 }
+        ]
+    },
+    {
+        itemCode: 'B1003',
+        itemName: 'Turkey Burger',
+        price: [
+            { Price:1600.00, discount:0 }
+        ]
+    },
+    {
+        itemCode: 'B1004',
+        itemName: 'Chicken Burger (Large)',
+        price: [
+            { Price:1400.00, discount:0 }
+        ]
+    },
+    {
+        itemCode: 'B1005',
+        itemName: 'Chicken Burger (Regular) ',
+        price: [
+            { Price:800.00, discount:20 }
+        ]
+    },
+    {
+        itemCode: 'B1006',
+        itemName: 'Cheese Burger (Large) ',
+        price: [
+            { Price:1000.00, discount:0 }
+        ]
+    },
+    {
+        itemCode: 'B1007',
+        itemName: 'Cheese Burger (Regular)',
+        price: [
+            { Price:600.00, discount:0 }
+        ]
+    },
+    {
+        itemCode: 'B1008',
+        itemName: 'Bacon Burger',
+        price: [
+            { Price:650.00, discount:15 }
+        ]
+    },
+    {
+        itemCode: 'B1009',
+        itemName: 'Shawarma Burger',
+        price: [
+            { Price:800.00, discount:0 }
+        ]
+    },
+    {
+        itemCode: 'B1010',
+        itemName: 'Olive Burger',
+        price: [
+            { Price:1800.00, discount:0 }
+        ]
+    },
+    {
+        itemCode: 'B1012',
+        itemName: 'Double-Cheese Burger',
+        price: [
+            { Price:1250.00, discount:20 }
+        ]
+    },
+    {
+        itemCode: 'B1013',
+        itemName: 'Crispy Chicken Burger (Regular)',
+        price: [
+            { Price:1200.00, discount:0 }
+        ]
+    },
+    {
+        itemCode: 'B1014',
+        itemName: 'Crispy Chicken Burger (Large)',
+        price: [
+            { Price:1600.00, discount:10 }
+        ]
+    },
+    {
+        itemCode: 'B1015',
+        itemName: 'Paneer Burger',
+        price: [
+            { Price:900.00, discount:0 }
+        ]
+    }
+];
+
+const submarineList = [
+    {
+        itemCode: 'B1016',
+        itemName: 'Crispy Chicken Submarine (Large) ',
+        price: [
+            { Price:2000.00, discount:0 }
+        ]
+    },
+    {
+        itemCode: 'B1017',
+        itemName: 'Crispy Chicken Submarine (Regular) ',
+        price: [
+            { Price:1500.00, discount:0 }
+        ]
+    },
+    {
+        itemCode: 'B1018',
+        itemName: 'Chicken Submarine (Large) ',
+        price: [
+            { Price:1800.00, discount:3 }
+        ]
+    },
+    {
+        itemCode: 'B1019',
+        itemName: 'Chicken Submarine (Regular)',
+        price: [
+            { Price:1400.00, discount:0 }
+        ]
+    },
+    {
+        itemCode: 'B1020',
+        itemName: 'Grinder Submarine ',
+        price: [
+            { Price:2300.00, discount:0 }
+        ]
+    },
+    {
+        itemCode: 'B1021',
+        itemName: 'Cheese Submarine',
+        price: [
+            { Price:22000.00, discount:0 }
+        ]
+    },
+    {
+        itemCode: 'B1022',
+        itemName: 'Double Cheese n Chicken Submarine',
+        price: [
+            { Price:1900.00, discount:16 }
+        ]
+    },
+    {
+        itemCode: 'B1023',
+        itemName: 'Special Horgie Submarine',
+        price: [
+            { Price:2800.00, discount:0 }
+        ]
+    },
+    {
+        itemCode: 'B1024',
+        itemName: 'MOS Special Submarine ',
+        price: [
+            { Price:3000.00, discount:0 }
+        ]
+    }
+];
+
+
+const FriesList = [
+    {
+        itemCode: 'B1025',
+        itemName: 'Steak Fries (Large) ',
+        price: [
+            { Price:1200.00, discount:0 }
+        ]
+    },
+    {
+        itemCode: 'B1026',
+        itemName: 'Steak Fries (Medium) ',
+        price: [
+            { Price:600.00, discount:0 }
+        ]
+    },
+    {
+        itemCode: 'B1027',
+        itemName: 'French Fries (Large)  ',
+        price: [
+            { Price:800.00, discount:3 }
+        ]
+    },
+    {
+        itemCode: 'B1028',
+        itemName: 'French Fries (Medium) ',
+        price: [
+            { Price:650.00, discount:0 }
+        ]
+    },
+    {
+        itemCode: 'B1029',
+        itemName: 'French Fries (Small)',
+        price: [
+            { Price:450.00, discount:0 }
+        ]
+    },
+    {
+        itemCode: 'B1030',
+        itemName: 'Sweet Potato Fries (Large)',
+        price: [
+            { Price:600.00, discount:0 }
+        ]
+    }
+];
+
+
+const PastaList = [
+    {
+        itemCode: 'B1031',
+        itemName: 'Chicken n Cheese Pasta ',
+        price: [
+            { Price:1600.00, discount:15 }
+        ]
+    },
+    {
+        itemCode: 'B1032',
+        itemName: 'Chicken Penne Pasta ',
+        price: [
+            { Price:1700.00, discount:0 }
+        ]
+    },
+    {
+        itemCode: 'B1033',
+        itemName: 'Ground Turkey Pasta Bake ',
+        price: [
+            { Price:2900.00, discount:10 }
+        ]
+    },
+    {
+        itemCode: 'B1034',
+        itemName: 'Creamy Shrimp Pasta',
+        price: [
+            { Price:2000.00, discount:0 }
+        ]
+    },
+    {
+        itemCode: 'B1035',
+        itemName: 'Lemon Butter Pasta',
+        price: [
+            { Price:1950.00, discount:0 }
+        ]
+    },
+    {
+        itemCode: 'B1036',
+        itemName: 'Tagliatelle Pasta',
+        price: [
+            { Price:2400.00, discount:1 }
+        ]
+    },
+    {
+        itemCode: 'B1037',
+        itemName: 'Baked Ravioli',
+        price: [
+            { Price:2000.00, discount:1 }
+        ]
+    }
+];
+
+const ChickenList = [
+    {
+        itemCode: 'B1038',
+        itemName: 'Fried Chicken (Small) ',
+        price: [
+            { Price:1200.00, discount:0 }
+        ]
+    },
+    {
+        itemCode: 'B1039',
+        itemName: 'Fried Chicken (Regular)',
+        price: [
+            { Price:2300.00, discount:10 }
+        ]
+    },
+    {
+        itemCode: 'B1040',
+        itemName: 'Fried Chicken (Large)',
+        price: [
+            { Price:3100.00, discount:5 }
+        ]
+    },
+    {
+        itemCode: 'B1041',
+        itemName: 'Hot Wings (Large)',
+        price: [
+            { Price:2400.00, discount:0 }
+        ]
+    },
+    {
+        itemCode: 'B1042',
+        itemName: 'Devilled Chicken (Large)',
+        price: [
+            { Price:900.00, discount:0 }
+        ]
+    },
+    {
+        itemCode: 'B1043',
+        itemName: 'BBQ Chicken (Regular)',
+        price: [
+            { Price:2100.00, discount:1 }
+        ]
+    }
+];
+
+const BeveragesList = [
+    {
+        itemCode: 'B1044',
+        itemName: 'Pepsi (330ml) ',
+        price: [
+            { Price:990.00, discount:5 }
+        ]
+    },
+    {
+        itemCode: 'B1045',
+        itemName: 'Coca-Cola (330ml)',
+        price: [
+            { Price:1230.00, discount:0 }
+        ]
+    },
+    {
+        itemCode: 'B1046',
+        itemName: 'Sprite (330ml)',
+        price: [
+            { Price:1500.00, discount:3 }
+        ]
+    },
+    {
+        itemCode: 'B1047',
+        itemName: 'Mirinda (330ml)',
+        price: [
+            { Price:850.00, discount:7 }
+        ]
+    }
+];
+
+//================Order List ==================
+
+const OrderList=[
+    {
+        OrderID:'OR001',
+        CustomerTele:'077',
+        CustomerName:'Kamal',
+        OrderDate:'2024-02-02',
+        OrderTime:'09.30.30',
+        OredrTotal:20000,
+        OrderDiscount:10,
+        OrderIteams:
+                [{
+                    IteamCode:'B001',
+                    PriceForIteam:2000.00,
+                    Discount:0,
+                    Quantity:1
+                }]
+            
+    },
+    {
+        OrderID:'OR001',
+        CustomerTele:'077',
+        CustomerName:'Kamal',
+        OrderDate:'2024-02-02',
+        OrderTime:'09.30.30',
+        OredrTotal:20000,
+        OrderDiscount:10,
+        OrderIteams:
+                [{
+                    IteamCode:'B001',
+                    PriceForIteam:2000.00,
+                    Discount:0,
+                    Quantity:1
+                }]
+            
+    },
+
+];
+
+
+
+
+
+
+
+
+
+//===========Store genarate Code============
+let BurgerHTML=``;
+let submarineHTML=``;
+let FriesHTML=``;
+let PastaHTML=``;
+let ChickenHTML=``;
+let BeveragesHTML=``;
+
+
+function genarateList() {
+    // Burgers
+    BurgersList.forEach((iteam) => {
+        let R;
+        iteam.price.forEach((price)=>{
+            R=price.Price;
+        })
+        BurgerHTML += `
+            <div class="card col-sm-12  mt-3  col-lg-4    col-md-3 " style="width: 18rem;">
+                <img src="asset/classicburger.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${iteam.itemName}</h5>
+                    <p class="card-text">
+                        Amount :${R}
+                    </p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                 </div>
+             </div> 
+        `;
+        
+    });
+    
+    // Submarines
+    submarineList.forEach((iteam) => {
+        let R;
+        iteam.price.forEach((price)=>{
+            R=price.Price;
+        })
+        submarineHTML += `
+            <div class="card col-sm-12  mt-3  col-lg-4    col-md-3 " style="width: 18rem;">
+                <img src="asset/Crispy Chicken Submarine.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${iteam.itemName}</h5>
+                    <p class="card-text">
+                        Amount :${R}
+                    </p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                 </div>
+             </div> 
+        `;
+        
+    });
+    
+    // Fries
+    FriesList.forEach((iteam) => {
+        let R;
+        iteam.price.forEach((price)=>{
+            R=price.Price;
+        })
+        FriesHTML += `
+            <div class="card col-sm-12  mt-3  col-lg-4    col-md-3 " style="width: 18rem;">
+                <img src="asset/Steak Fries.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${iteam.itemName}</h5>
+                    <p class="card-text">
+                        Amount :${R}
+                    </p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                 </div>
+             </div> 
+        `;
+        
+    });
+
+    // Pasta
+    PastaList.forEach((iteam) => {
+        let R;
+        iteam.price.forEach((price)=>{
+            R=price.Price;
+        })
+        PastaHTML += `
+            <div class="card col-sm-12  mt-3  col-lg-4    col-md-3 " style="width: 18rem;">
+                <img src="asset/Chicken n Cheese Pasta .png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${iteam.itemName}</h5>
+                    <p class="card-text">
+                        Amount :${R}
+                    </p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                 </div>
+             </div> 
+        `;
+        
+    });
+    
+    // Chicken
+    ChickenList.forEach((iteam) => {
+        let R;
+        iteam.price.forEach((price)=>{
+            R=price.Price;
+        })
+        ChickenHTML += `
+            <div class="card col-sm-12  mt-3  col-lg-4    col-md-3 " style="width: 18rem;">
+                <img src="asset/Fried Chicken.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${iteam.itemName}</h5>
+                    <p class="card-text">
+                        Amount :${R}
+                    </p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                 </div>
+             </div> 
+        `;
+        
+    });
+    
+    // Bevarage
+    BeveragesList.forEach((iteam) => {
+        let R;
+        iteam.price.forEach((price)=>{
+            R=price.Price;
+        })
+        BeveragesHTML += `
+            <div class="card col-sm-12  mt-3  col-lg-4    col-md-3 " style="width: 18rem;">
+                <img src="asset/pepsi.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${iteam.itemName}</h5>
+                    <p class="card-text">
+                        Amount :${R}
+                    </p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                 </div>
+             </div> 
+        `;
+        
+    });
+        
+}
+
+function display() {
+    const BeveragesElement=document.querySelector('.js-iteam-Beverages');
+    const ChickenElement=document.querySelector('.js-iteam-Chicken');
+    const PastaElement=document.querySelector('.js-iteam-Pasta');
+    const FriesElement=document.querySelector('.js-iteam-Fries');
+    const submarineElement=document.querySelector('.js-iteam-submarine');
+    const BurgerElement=document.querySelector('.js-iteam-burgers');
+
+    BurgerElement.innerHTML=BurgerHTML;
+    submarineElement.innerHTML=submarineHTML;
+    FriesElement.innerHTML=FriesHTML;
+    PastaElement.innerHTML=PastaHTML;
+    ChickenElement.innerHTML=ChickenHTML;
+    BeveragesElement.innerHTML=BeveragesHTML;    
+}
+
+
+genarateList();
+display();
